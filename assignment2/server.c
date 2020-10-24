d5f0ad0642304a0b9f22ff5d6c7c5b0b73ab6b72
cBrayton <charlie.brayton@gmail.com>
2020-10-24 16:20:39 -0700

    Add files via upload
    Socket only listens and accepts in new process

--- assignment2/server.c
+++ assignment2/server.c
@@ -16,45 +16,13 @@ int main(int argc, char const *argv[])
     int addrlen = sizeof(address);
     char buffer[102] = {0};
     char *hello = "Hello from server";
-    
-    int id = fork();
-    struct passwd *nobody = getpwnam("nobody");
-    setuid(nobody->pw_uid); //Set both parent and child to nobody 
-                            //Parent doesn't need root either
-    printf("execve=0x%p\n", execve);
-    //Two prints to see if it changed after fork
 
-    if(id == 0) {
-        char *args[]={"./server", "-cp"};
-        execvp(args[0],args);
-    }
+    printf("Starting Server\n");
+ 
+    if(strcmp(argv[argc-1], "-cp") == 0 && argc >= 2) {
 
-    if(argv[argc-1] == "-cp") {
-        // Creating socket file descriptor
-        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-        {
-            perror("socket failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Forcefully attaching socket to the port 8080
-        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                                                  &opt, sizeof(opt)))
-        {
-            perror("setsockopt");
-            exit(EXIT_FAILURE);
-        }
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY;
-        address.sin_port = htons( PORT );
-
-        // Forcefully attaching socket to the port 8080
-        if (bind(server_fd, (struct sockaddr *)&address,
-                                 sizeof(address))<0)
-        {
-            perror("bind failed");
-            exit(EXIT_FAILURE);
-        }
+        server_fd = atoi(argv[argc-2]);
+        //Only start listening to socket in new proccess
         if (listen(server_fd, 3) < 0)
         {
             perror("listen");
@@ -71,6 +39,56 @@ int main(int argc, char const *argv[])
         send(new_socket , hello , strlen(hello) , 0 );
         printf("Hello message sent\n");
     }
-    else {wait();}
+
+    else {   
+        int id = fork();
+        struct passwd *nobody = getpwnam("nobody");
+        setuid(nobody->pw_uid); //Set both parent and child to nobody 
+                               //Parent doesn't need root either
+        printf("Id is: %i\n", id);
+
+        if(id == 0) {
+
+            // Creating socket file descriptor
+            if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+            {
+                perror("socket failed");
+                exit(EXIT_FAILURE);
+            }
+        
+            printf("Socket is: %d\n",server_fd);
+
+            // Forcefully attaching socket to the port 8080
+            if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+            {
+                perror("setsockopt");
+                exit(EXIT_FAILURE);
+            }
+            address.sin_family = AF_INET;
+            address.sin_addr.s_addr = INADDR_ANY;
+            address.sin_port = htons( PORT );
+
+            // Forcefully attaching socket to the port 8080
+            if (bind(server_fd, (struct sockaddr *)&address,
+                                 sizeof(address))<0)
+            {
+                perror("bind failed");
+                exit(EXIT_FAILURE);
+            }
+
+            char server_fd_char[10];
+            sprintf(server_fd_char,"%d",server_fd);
+            char *args[]={"./server", server_fd_char , "-cp", NULL};
+            printf("Re-execing with args: %s, %s, %s\n", args[0], args[1], args[2]);
+            if(execvp(args[0],args) < 1) {
+                printf("Exec failed\n");
+                exit(1);
+            }
+
+        }
+
+        else {wait();}
+    }
     return 0;
 }
